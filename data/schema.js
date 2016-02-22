eb8e6d1873d4af6c3f8437fec7dcb5440369cbe9
ulfarj <ulfarj@gmail.com>
2016-02-22 23:05:11 +0000

    ss

--- data/schema.js
+++ data/schema.js
@@ -16,9 +16,45 @@ let Schema = (db) => {
 			categories: {
 				type: new GraphQLList(categoryType),					
 				resolve: () => db.collection('categories').find({}).toArray()
+			},
+			companies: {
+				type: new GraphQLList(companyType),
+				resolve: () => db.collection('companies').find({}).toArray()
+			},
+			salesmen: {
+				type: new GraphQLList(salesmanType),
+				resolve: () => db.collection('salesmen').find({}).toArray()
+			},
+			sales: {
+				type: new GraphQLList(saleType),
+				resolve: () => db.collection('sales').find({}).toArray()
 			}
 		})
-	});		
+	});
+
+	let saleType =  new GraphQLObjectType({
+		name: 'Sale',
+		fields: () => ({
+			_id: {type: GraphQLString},
+			company_id: {type: GraphQLString},
+			salesman_id: {type: GraphQLString},
+			category_id: {type: GraphQLString}
+		})
+	})
+
+	let companyType = new GraphQLObjectType({
+		name: 'Company',
+		fields: () => ({
+			_id: {type: GraphQLString},
+			name: {type: GraphQLString},
+			ssn: {type: GraphQLString},
+			address: {type: GraphQLString},
+			postalCode: {type: GraphQLString},
+			phone: {type: GraphQLString},
+			email: {type: GraphQLString},
+			comment: {type: GraphQLString}
+		})
+	});	
 		
 	let categoryType = new GraphQLObjectType({
 		name: 'Category',
@@ -27,6 +63,15 @@ let Schema = (db) => {
 			name: {type: GraphQLString}
 		})
 	});
+
+	let salesmanType = new GraphQLObjectType({
+		name: 'Salesman',
+		fields: () => ({
+			_id: {type: GraphQLString},
+			name: {type: GraphQLString}
+		})
+	});
+
 
 	let schema = new GraphQLSchema({
 		query: new GraphQLObjectType({
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Relay from "react-relay";
+import {Table, Input, Fade, Button, Well, ButtonToolbar, Overlay, Popover, 
+Panel, Modal, Grid, Row, Col} from 'react-bootstrap';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';  
+import _ from 'lodash';
 
 import Category from './Category'; 
+
+const styles = {
+  input: {
+    border: 0        
+  },
+  tbody: {
+    border: 0
+  },
+  gridArea: {
+    paddingTop: 20
+  },
+  headerArea: {
+    paddingTop: 10,
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  editDialog: {
+    width: '90%'
+  }
+};
 
 export default class Main extends React.Component {
 	constructor(props) {
 	    super(props);
-
-	    //this.state = _getAppState();
-	    //this.onChange = this.onChange.bind(this);
   	}
 
   	render() {
